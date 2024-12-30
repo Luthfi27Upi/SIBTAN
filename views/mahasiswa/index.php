e5160017f4649dde2b54c0daebd8139864877941
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-12-30 07:30:53 +0700

    fix fe mhs & logout

--- views/mahasiswa/index.php
+++ views/mahasiswa/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,55 +9,49 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <style>
-    /* Sidebar styling */ 
-    h5{
-      margin-top: -20px; /* mengatur jarak logo dan sibtan */
+    h5 {
+      margin-top: -20px;
     }
 
     header {
-    background-color: #CCE5FF; /* Warna biru muda */
-    text-align: center; 
-    color: #043873; /* Warna teks biru gelap */
-    font-size: 1.5rem;
-    font-weight: 500;
-    padding: 15px; /* mengatur ruang didalam elemen */
-    border-radius: 40px; /* Sudut melingkar */
-    width: 80%; /* Lebar header agar proporsional */
-    margin-left: 230px;  /*memberi jarak dari sisi kiri */
+      background-color: #CCE5FF;
+      text-align: center;
+      color: #043873;
+      font-size: 1.5rem;
+      font-weight: 500;
+      padding: 15px;
+      border-radius: 40px;
+
     }
 
-    /* Main Content styling */
     .content {
-      margin-left: 10px; 
+      margin-left: 10px;
       width: calc(100% - 200px);
       padding: 20px;
-     
+
     }
 
     .hero-section {
-      background: url('../img/RuanganDashboard.png') center/cover no-repeat;
+      background: url('../../img/RuanganDashboard.png') center/cover no-repeat;
       padding: 100px 20px;
       border-radius: 30px;
-      margin-left: 200px;
     }
 
     .hero-section h2 {
-    color: #FFE492; /* Warna teks */
-    text-align: center; /* Tengah */
-    font-size: 3rem; /* Ukuran font lebih besar */
-    font-weight: bold; /* Teks lebih tebal */
-  }
+      color: #FFE492;
+      text-align: center;
+      font-size: 3rem;
+      font-weight: bold;
+    }
 
-  .hero-section p {
-    color: #FFE492;
-    font-size: 1.5rem; /* Ukuran font lebih besar */
-    font-weight: 500; /* Tebal sedang */
-  }
-    
-
-
+    .hero-section p {
+      color: #FFE492;
+      font-size: 1.5rem;
+      font-weight: 500;
+    }
   </style>
 </head>
+
 <body class="d-flex flex-column min-vh-100">
   <div class="d-flex flex-grow-1">
     <!-- Sidebar -->
@@ -69,13 +64,13 @@
       </header>
       <div class="p-4">
         <div class="hero-section text-center mb-4">
-          <h2 style="color:#FFE492; text-align:center;" >Sistem Bebas Tanggungan</h2>
+          <h2 style="color:#FFE492; text-align:center;">Sistem Bebas Tanggungan</h2>
           <p style="color:#FFE492">Jurusan Teknologi Informasi</p>
           <!--<img src="img/RuanganDashboard.png" class="img-fluid rounded mx-auto d-block background-cover-img" style="max-width: 600px;" alt="Ruangan Dashboard">-->
         </div>
         <section class="info-section text-center">
-          <h4 style="color:#043873; margin-left: 200px;" >Profil Sistem Bebas Tanggungan</h4>
-          <p style="color:#043873; font-size: 14px; margin-left: 200px;">
+          <h4 style="color:#043873;">Profil Sistem Bebas Tanggungan</h4>
+          <p style="color:#043873; font-size: 14px;">
             Sistem "bebas tanggungan" adalah istilah yang biasanya merujuk pada suatu bentuk sistem
             yang tidak menuntut adanya tanggung jawab atau kewajiban yang berkelanjutan dari pihak tertentu.
             Dalam konteks administrasi, keuangan, atau hukum, sistem bebas tanggungan berarti bahwa pihak-pihak
@@ -89,4 +84,5 @@
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
